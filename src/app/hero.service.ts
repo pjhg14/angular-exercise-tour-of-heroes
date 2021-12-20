6611e7af31d0e7e742f361000cf92e67e5ae296c
pjhg14 <pjhg14@gmail.com>
2021-12-19 20:52:42 -0500

    completed Add Navigation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,4 +16,11 @@ export class HeroService {
     
     return heroes
   }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(hero => hero.id === id)!
+    this.messageService.add(`HeroService: fetched hero id=${id}`)
+
+    return of(hero)
+  }
 }
